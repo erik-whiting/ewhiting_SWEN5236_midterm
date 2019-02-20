0940863e585044b045c9ea778d9884ce5e1c5c13
erik-whiting <erik@erikwhiting.com>
2019-02-19 18:28:57 -0600

    some commtis and tests

---
+++ Database/DB.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: eedee
+ * Date: 2/18/2019
+ * Time: 11:57 PM
+ */
+
+class DB
+{
+    public $servername = "localhost";
+    public $username = "erik@localhost";
+    public $db_name = "ewhiting_midterm";
+    public $password = "Eagle200";
+
+    function connect() {
+        try {
+            $conn = new PDO("mysql:host=$this->servername;dbname=$this->db_name", $this->username, getenv("DB_PASSWORD"));
+            return $conn;
+        } catch(PDOException $e)
+        {
+            return "Connection failed: " . $e->getMessage();
+        }
+    }
+
+}
