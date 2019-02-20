b668b4f1f8bdb4cf3e7b42775545c83b8701c610
erik-whiting <erik@erikwhiting.com>
2019-02-19 19:08:39 -0600

    Update db_test.php

--- db_test.php
+++ db_test.php
@@ -2,12 +2,34 @@
 
 require_once "Database/DB.php";
 
+echo "<table style='border: solid 1px black;'>";
+echo "<tr><th>Id</th><th>Rating</th></tr>";
+
+class TableRows extends RecursiveIteratorIterator {
+    function __construct($it) {
+        parent::__construct($it, self::LEAVES_ONLY);
+    }
+
+    function current() {
+        return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
+    }
+
+    function beginChildren() {
+        echo "<tr>";
+    }
+
+    function endChildren() {
+        echo "</tr>" . "\n";
+    }
+}
+
 $db = new DB();
 $conn = new PDO($db->getConnectionString(), $db->username, $db->password);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $stmt = $conn->prepare("SELECT * FROM Rating");
 $stmt->execute();
 
 $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-foreach ($stmt->fetchAll() as $k=>$v) {
-    print $v;
+foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+    echo $v;
 }
