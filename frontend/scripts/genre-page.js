ab7d6df68c80496ae16f80f6746cba25c9b7baee
erik-whiting <erik@erikwhiting.com>
2019-02-21 18:56:53 -0600

    Added pictures

--- frontend/scripts/genre-page.js
+++ frontend/scripts/genre-page.js
@@ -2,9 +2,12 @@ genres = Array();
 url = 'http://ewhiting.eastus.cloudapp.azure.com/midterm/Classes/Genre/read.php';
 $.get(url, function(data, status) {
     var records = data.records;
-    records.forEach(function(record) {
+    records.forEach(function(record, i) {
         var genre = new Genre(record);
         genres.push(record);
+        document.getElementById(genre.name).innerText = genre.name;
+        var img_id = genre.name + '-img';
+        document.getElementById(img_id).src = genre.picture_path;
     })
 });
 
@@ -14,4 +17,9 @@ class Genre {
         this.name = build_object.name;
         this.picture_path = build_object.picture_path;
     }
+}
+document.getElementById("animation").innerText = genres[0]["name"];
+
+function return_values(index, value) {
+    return genres[index][value];
 }
