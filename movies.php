9adba6154ef835fd321f828e64e698d00e8b5482
erik-whiting <erik@erikwhiting.com>
2019-02-23 17:41:22 -0600

    Update movies.php

--- movies.php
+++ movies.php
@@ -5,7 +5,6 @@ include_once './Classes/Movie/full_data.php';
 
 $database = new DB();
 $db = $database->getConnection();
-$movies;
 
 $genre = new Genre($db);
 
@@ -14,19 +13,19 @@ $genre_id = $_GET['genre'];
 $stmt = $genre->getMovies($genre_id);
 $num = $stmt->rowcount();
 
-if ($num > 0) {
-    $results_array = array();
-    $results_array["movies"] = array();
-    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-        extract($row);
-        $result_item = getMovie($movie);
-        array_push($results_array["movies"], $result_item);
-    }
-    http_response_code(200);
-    echo json_encode($results_array);
-} else {
-    echo json_encode(array("message" => "Could not find requested data"));
-}
+//if ($num > 0) {
+//    $results_array = array();
+//    $results_array["movies"] = array();
+//    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+//        extract($row);
+//        $result_item = getMovie($movie);
+//        array_push($results_array["movies"], $result_item);
+//    }
+//    http_response_code(200);
+//    echo json_encode($results_array);
+//} else {
+//    echo json_encode(array("message" => "Could not find requested data"));
+//}
 
 ?>
 <!DOCTYPE html>
