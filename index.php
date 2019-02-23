782b233ea36ee4474e15bdff696cec356ab99e42
erik-whiting <erik@erikwhiting.com>
2019-02-23 15:17:26 -0600

    Update index.php

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 
-    <link rel="stylesheet" href="css/style.css">
-    <script src="scripts/genre-page.js"></script>
+    <link rel="stylesheet" href="frontend/css/style.css">
+    <script src="frontend/scripts/genre-page.js"></script>
 
 </head>
 <body>
