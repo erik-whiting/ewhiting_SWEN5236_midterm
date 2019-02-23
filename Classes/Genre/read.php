a52024cccc0ca603b5b15aedeed3b5e2bff34bd6
erik-whiting <erik@erikwhiting.com>
2019-02-23 16:54:32 -0600

    Update read.php

--- Classes/Genre/read.php
+++ Classes/Genre/read.php
@@ -5,29 +5,25 @@ include_once 'Genre.php';
 $database = new DB();
 $db = $database->getConnection();
 
-function getGenres() {
-    $genre = new Genre($this->db);
+$genre = new Genre($db);
 
-    $stmt = $genre->read();
-    $num = $stmt->rowCount();
+$stmt = $genre->read();
+$num = $stmt->rowCount();
 
-    if($num>0) {
-        $results_array = array();
-        $results_array["records"]=array();
-        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-            extract($row);
-            $result_item=array(
-                "id" => $id,
-                "name" => $name,
-                "picture_path" => $picture_path
-            );
-            array_push($results_array["records"], $result_item);
-        }
-        return $results_array["records"];
-    } else {
-        http_response_code(404);
-        echo json_encode(array("message" => "No records found."));
+if($num>0) {
+    $results_array = array();
+    $results_array["records"]=array();
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        extract($row);
+        $result_item=array(
+            "id" => $id,
+            "name" => $name,
+            "picture_path" => $picture_path
+        );
+        array_push($results_array["records"], $result_item);
     }
+    return json_encode($results_array);
+} else {
+    http_response_code(404);
+    echo json_encode(array("message" => "No records found."));
 }
-
-echo getGenres();
