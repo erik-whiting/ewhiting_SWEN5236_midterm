0aeacaad9ca79ad3f6da291058efbb262823aeb3
erik-whiting <erik@erikwhiting.com>
2019-02-23 16:03:41 -0600

    ;lkjlkjlkj;lajsdkfa

--- Classes/Genre/read.php
+++ Classes/Genre/read.php
@@ -1,32 +1,30 @@
 <?php
-function getGenres() {
-    include_once  '../header.php';
-    include_once 'Genre.php';
+include_once  '../header.php';
+include_once 'Genre.php';
 
-    $database = new DB();
-    $db = $database->getConnection();
+$database = new DB();
+$db = $database->getConnection();
 
-    $genre = new Genre($db);
+$genre = new Genre($db);
 
-    $stmt = $genre->read();
-    $num = $stmt->rowCount();
+$stmt = $genre->read();
+$num = $stmt->rowCount();
 
-    if($num>0) {
-        $results_array = array();
-        $results_array["records"]=array();
-        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-            extract($row);
-            $result_item=array(
-                "id" => $id,
-                "name" => $name,
-                "picture_path" => $picture_path
-            );
-            array_push($results_array["records"], $result_item);
-        }
-        http_response_code(200);
-        return $results_array;
-    } else {
-        http_response_code(404);
-        return json_encode(array("message" => "No records found."));
+if($num>0) {
+    $results_array = array();
+    $results_array["records"]=array();
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        extract($row);
+        $result_item=array(
+            "id" => $id,
+            "name" => $name,
+            "picture_path" => $picture_path
+        );
+        array_push($results_array["records"], $result_item);
     }
+    http_response_code(200);
+    echo json_encode($results_array);
+} else {
+    http_response_code(404);
+    echo json_encode(array("message" => "No records found."));
 }
--- index.php
+++ index.php
@@ -21,13 +21,37 @@
 
 <div class="container">
     <div class="row">
-        <?php
-        include_once ('./Classes/Genre');
-            $genres = getGenres();
-            foreach ($genres as $genre) {
-                echo "<p>" . $genre.name . "</p>";
-        }
-        ?>
+
+        <script>
+            genres = Array();
+            url = 'http://ewhiting.eastus.cloudapp.azure.com/midterm/Classes/Genre/read.php';
+            $.get(url, function(data, status) {
+                var records = data.records;
+                records.forEach(function(record, i) {
+                    var genre = new Genre(record);
+                    genres.push(record);
+                    var genre_url = genre_service_url + genre.id;
+                    document.getElementById(genre.name).innerText = genre.name;
+                    var link_id = genre.name + '-link';
+                    document.getElementById(link_id).setAttribute('href', genre_url);
+                    var img_id = genre.name + '-img';
+                    document.getElementById(img_id).src = genre.picture_path;
+                })
+            });
+
+            class Genre {
+                constructor(build_object) {
+                    this.id = build_object.id;
+                    this.name = build_object.name;
+                    this.picture_path = build_object.picture_path;
+                }
+            }
+            document.getElementById("animation").innerText = genres[0]["name"];
+
+            function return_values(index, value) {
+                return genres[index][value];
+            }
+        </script>
 <!--        <div class="col-sm-4">-->
 <!--            <a id="Action-link">-->
 <!--            <div class="panel panel-primary">-->
