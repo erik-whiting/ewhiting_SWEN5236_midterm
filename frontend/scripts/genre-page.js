57647d9d026cd569f3c2d7149ffc8fb96216210c
erik-whiting <erik@erikwhiting.com>
2019-02-23 12:58:03 -0600

    Added linking to front end

--- frontend/scripts/genre-page.js
+++ frontend/scripts/genre-page.js
@@ -1,11 +1,15 @@
 genres = Array();
 url = 'http://ewhiting.eastus.cloudapp.azure.com/midterm/Classes/Genre/read.php';
+genre_service_url = 'http://ewhiting.eastus.cloudapp.azure.com/midterm/Classes/Genre/movie_by_genre.php/?genre=';
 $.get(url, function(data, status) {
     var records = data.records;
     records.forEach(function(record, i) {
         var genre = new Genre(record);
         genres.push(record);
+        var genre_url = genre_service_url + genre.id;
         document.getElementById(genre.name).innerText = genre.name;
+        var link_id = genre.name + '-link';
+        document.getElementById(link_id).setAttribute('href', genre_url);
         var img_id = genre.name + '-img';
         document.getElementById(img_id).src = genre.picture_path;
     })
