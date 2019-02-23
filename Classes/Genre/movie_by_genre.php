30cb0bdb2264eff55de722ff721e7d12a0e03e5a
erik-whiting <erik@erikwhiting.com>
2019-02-23 12:34:31 -0600

    starting movies array service

--- Classes/Genre/movie_by_genre.php
+++ Classes/Genre/movie_by_genre.php
@@ -1,47 +1,28 @@
 <?php
 include_once '../header.php';
 include_once 'Genre.php';
-include_once '../Movie/Movie.php';
+include_once '../Movie/full_data.php';
 
 $database = new DB();
 $db = $database->getConnection();
 
 $genre = new Genre($db);
-$movie = new Movie($db);
+
 $genre_id = $_GET['genre'];
 
 $stmt = $genre->getMovies($genre_id);
 $num = $stmt->rowcount();
 
-if ($num>0) {
+if ($num > 0) {
     $results_array = array();
     $results_array["movies"] = array();
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-        $movie_stmt = $movie->full_data($row["movie"]);
-        $movie_num = $movie_stmt->rowcount();
-        if ($movie_num > 0) {
-            while ($movie_row = $movie_stmt->fetch(PDO::FETCH_ASSOC)) {
-                extract($movie_row);
-                $result_item = array (
-                    "name" => $name,
-                    "from" => $from,
-                    "to" => $to,
-                    "rating" => $rating,
-                    "director" => $director,
-                    "description" => $description,
-                    "votes" => $votes,
-                    "gross" => $gross
-                );
-                array_push($results_array["movie"], $result_item);
-            }
-        }
-        else {
-            echo json_encode(array("message" => "Couldn't find any movies in that genre"));
-        }
+        extract($row);
+        $result_item = getMovie($movie);
+        array_push($results_array, $result_item);
     }
     http_response_code(200);
     echo json_encode($results_array);
 } else {
-    http_response_code(400);
-    echo json_encode(array("message" => "No records found"));
+    echo json_encode(array("message" => "Could not find requested data"));
 }
--- Classes/Movie/full_data.php
+++ Classes/Movie/full_data.php
@@ -2,39 +2,44 @@
 include_once('../header.php');
 include_once ('Movie.php');
 
-$database = new DB();
-$db = $database->getConnection();
 
-$movie = new Movie($db);
 
-$stmt = $movie->full_data(1);
-$num = $stmt->rowcount();
+function getMovie($movie_id) {
+    $database = new DB();
+    $db = $database->getConnection();
 
-$rating = $movie->get_vote_info(1);
-$rnum = $rating->rowcount();
+    $movie = new Movie($db);
 
-if ($num>0) {
-    $results_array = array();
-    $results_array["movie"]=array();
-    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-        extract($row);
-        extract($rating->fetch(PDO::FETCH_ASSOC));
-        $result_item = array(
-            "id" => $id,
-            "name" => $name,
-            "from" => $from,
-            "to" => $to,
-            "rating" => number_format($avg, 2),
-            "description" => $description,
-            "director" => $director,
-            "votes" => $votes,
-            "gross" => $gross
-        );
-        array_push($results_array["movie"], $result_item);
+    $stmt = $movie->full_data(1);
+    $num = $stmt->rowcount();
+
+    $rating = $movie->get_vote_info(1);
+    $rnum = $rating->rowcount();
+
+    if ($num>0) {
+        $results_array = array();
+        $results_array["movie"]=array();
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            extract($row);
+            extract($rating->fetch(PDO::FETCH_ASSOC));
+            $result_item = array(
+                "id" => $id,
+                "name" => $name,
+                "from" => $from,
+                "to" => $to,
+                "rating" => number_format($avg, 2),
+                "description" => $description,
+                "director" => $director,
+                "votes" => $votes,
+                "gross" => $gross
+            );
+            array_push($results_array["movie"], $result_item);
+        }
+        http_response_code(200);
+        echo json_encode($results_array);
+        return json_encode($results_array);
+    } else {
+        echo json_encode(array("message" => "No record found for that movie"));
     }
-    http_response_code(200);
-    echo json_encode($results_array);
-    return json_encode($results_array);
-} else {
-    echo json_encode(array("message" => "No record found for that movie"));
 }
+
