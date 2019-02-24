7691da47a21f3f124c62a808df3599b4d2b3c0ec
erik-whiting <erik@erikwhiting.com>
2019-02-23 23:24:36 -0600

    Update index.php

--- index.php
+++ index.php
@@ -13,6 +13,7 @@ ini_set('display_errors', 1);
 
     <link rel="stylesheet" href="frontend/css/style.css">
     <script src="frontend/scripts/genre-page.js"></script>
+    <script src="frontend/scripts/cart.js"></script>
 
 </head>
 <body>
