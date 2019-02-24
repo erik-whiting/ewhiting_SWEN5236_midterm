3842c5f742271e85f000ec9a6cbea00bf1f1d2da
erik-whiting <erik@erikwhiting.com>
2019-02-23 20:14:27 -0600

    Update movies.php

--- movies.php
+++ movies.php
@@ -23,7 +23,7 @@ ini_set('display_errors', 1);
 
 </div>
 <div id="content">
-    <script>$(document).ready(function( setContent();) ) {}</script>
+    <script>$(document).ready(function() { setContent(); }</script>
 </div>
 </body>
 </html>
