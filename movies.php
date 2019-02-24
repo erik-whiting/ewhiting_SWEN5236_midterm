5fbbb0bc3c66e6668d12085b15fca038ac12553f
erik-whiting <erik@erikwhiting.com>
2019-02-23 18:51:22 -0600

    Update movies.php

--- movies.php
+++ movies.php
@@ -1,11 +1,3 @@
-<?php
-
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-?>
-<?php //function TestBlockHTML ($replStr) {ob_start();
- ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -27,5 +19,4 @@ ini_set('display_errors', 1);
 </div>
 </body>
 </html>
-<?php //return ob_get_clean(); } 
-?>
+
