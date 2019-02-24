951a66f42d180232e3f3140f399d7d714b14d720
erik-whiting <erik@erikwhiting.com>
2019-02-23 18:52:59 -0600

    Update by-genre-page.js

--- frontend/scripts/by-genre-page.js
+++ frontend/scripts/by-genre-page.js
@@ -1,15 +1,15 @@
 movies = Array();
 url = 'http://ewhiting.eastus.cloudapp.azure.com/midterm/Classes/Genre/movie_by_genre.php/?genre=';
 
-function getUrlVars() {
-    var vars = {};
-    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
-        vars[key] = value;
-    });
-    return vars;
-}
-
-var genre_id = getUrlVars()["genre"];
+// function getUrlVars() {
+//     var vars = {};
+//     var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+//         vars[key] = value;
+//     });
+//     return vars;
+// }
+//
+// var genre_id = getUrlVars()["genre"];
 url += 1;
 
 $.get(url, function(data, status) {
