9ca417f84ec56bf512b247300f0192ffaee1a1d4
erik-whiting <erik@erikwhiting.com>
2019-02-23 22:12:38 -0600

    cart service

--- Classes/AppUser/AppUser.php
+++ Classes/AppUser/AppUser.php
@@ -27,4 +27,31 @@ class AppUser
         return $stmt;
     }
 
+    function getCart($user_id) {
+        $query = "SET @user_cart := (SEELCT id FROM Cart WHERE user_id = " . $user_id .
+            " AND active = 1;
+            SELECT m.name AS 'movie_name', m.price AS 'movie_price' FROM movie_cart mc
+            INNER JOIN Movie m
+            ON mc.movie_id = m.id
+            WHERE cart_id = @user_cart";
+        $stmt = $this->conn.prepare($query);
+        $stmt->exectute();
+
+        return $stmt;
+    }
+
+    function getCountAndPrice($user_id) {
+        $query = "SET @user_cart := (SEELCT id FROM Cart WHERE user_id = " . $user_id .
+            " AND active = 1;
+            SELECT COUNT(m.name) AS 'cart_count', 
+            SUM(m.price) AS 'cart_price' FROM movie_cart mc
+            INNER JOIN Movie m
+            ON mc.movie_id = m.id
+            WHERE cart_id = @user_cart";
+        $stmt = $this->conn.prepare($query);
+        $stmt->exectute();
+
+        return $stmt;
+    }
+
 }
---
+++ Classes/AppUser/get_cart.php
@@ -0,0 +1,34 @@
+<?php
+include_once '/home/erik/localcode/midterm/Classes/header.php';
+include_once '/home/erik/localcode/midterm/Classes/AppUser/AppUser.php';
+
+$database = new DB();
+$db = $database->getConnection();
+
+$user = new AppUser($db);
+$user_id = $_GET['user'];
+
+$stmt = $user->getCart($user_id);
+$num = $stmt->rowcount();
+$counts = $user->getCountAndPrice($user_id);
+
+if ($num > 0) {
+    $results_array = array();
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        extract($row);
+        extract($counts->fetch(PDO::FETCH_ASSOC));
+        $result_item = array(
+            "movie" => $movie_name,
+            "price" => $movie_price,
+            "cart_count" => $cart_count,
+            "subtotal" => $cart_price,
+            "tax" => number_format($cart_price * 0.08, 2),
+            "total" => number_format($cart_price + ($cart_price * 0.08), 2)
+        );
+        array_push($results_array, $result_item);
+    }
+    http_response_code(200);
+    echo json_encode(results_array);
+} else {
+    echo json_encode(array("message" => "This Cart is empty"));
+}
