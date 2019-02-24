03932647ffd5b34519849dbed1c3d7b31de9399b
erik-whiting <erik@erikwhiting.com>
2019-02-24 16:16:58 -0600

    Update cart-page.js

--- frontend/scripts/cart-page.js
+++ frontend/scripts/cart-page.js
@@ -24,10 +24,13 @@ class Item {
 
 class Receipt {
     constructor(receipt) {
-        this.subtotal = receipt.subtotal;
+        this.subtotal = receipt.subtotal.toFixed(2);
         this.cart_count = receipt.cart_count;
-        this.tax = receipt.tax;
-        this.total = receipt.total;
+        this.tax = receipt.tax.toFixed(2);
+        this.total = receipt.total.toFixed(2);
+        if (receipt.savings != null) {
+            this.savings = receipt.savings.toFixed(2);
+        }
     }
 }
 
