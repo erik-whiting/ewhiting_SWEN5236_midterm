3c5e878341259230da6258578fa7847e4cc61ec8
erik-whiting <erik@erikwhiting.com>
2019-02-23 19:38:26 -0600

    ;lkasdf

--- frontend/scripts/by-genre-page.js
+++ frontend/scripts/by-genre-page.js
@@ -10,11 +10,26 @@ function getUrlVars() {
 }
 
 var genre_id = getUrlVars()["genre"];
-url += genre_id;
+url += 1;
 
 $.get(url, function(data, status) {
     var records = data.movies;
     records.forEach(function (record, i) {
-        movies.push(record);
+        var movie = new Movie(record);
+        movies.push(movie);
     })
 });
+
+class Movie {
+    constructor(build_object) {
+        this.id = build_object.id;
+        this.name = build_object.name;
+        this.description = build_object.description;
+        this.from = build_object.from;
+        this.to = build_object.to;
+        this.rating = build_object.rating;
+        this.director = build_object.director;
+        this.votes = build_object.votes;
+        this.gross = build_object.gross;
+    }
+}
